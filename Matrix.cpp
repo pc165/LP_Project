9c1ec7e2391acefc6fed7af340bd80d189ab0c15
p014 <pc165@pm.me>
2019-09-28 00:44:32 +0200

    MatrixCSR::MatrixCSR(const MatrixCSR &e)

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,1 +1,25 @@
 #include "Matrix.h"
+
+MatrixCSR::MatrixCSR(const int &row, const int &col):col_(nullptr), value_(nullptr), rowIndex_(nullptr), nRow_(row), nCol_(col), sizeIndexRow_(0) {
+
+}
+
+MatrixCSR::MatrixCSR(const MatrixCSR &e) : nRow_(e.nRow_), nCol_(e.nCol_), sizeIndexRow_(e.sizeIndexRow_) {
+
+	int totalValuesCol = 0;
+
+	if (e.rowIndex_ != nullptr)
+		totalValuesCol = int(e.rowIndex_[sizeIndexRow_ - 1]); // is always accesible?
+	else throw "No es pot accedir a: e.rowIndex_[maxIndexRow_ - 1]";
+
+	value_ = new float[totalValuesCol];
+	col_ = new float[totalValuesCol];
+	rowIndex_ = new float[sizeIndexRow_];
+
+	for (int i = 0; i < totalValuesCol; i++) {
+		col_[i] = e.col_[i];
+		value_[i] = e.value_[i];
+	}
+	for (int i = 0; i < sizeIndexRow_; i++)
+		rowIndex_[i] = e.rowIndex_[i];
+}
--- Matrix.h
+++ Matrix.h
@@ -1,9 +1,32 @@
 #pragma once
-class Matrix {
-	Matrix():matrix_(nullptr), row_(0), col_(0) {}
+#include <vector>
+#include <iostream>
+class MatrixCSR {
+	MatrixCSR():col_(nullptr), value_(nullptr), rowIndex_(nullptr), nRow_(0), nCol_(0), sizeIndexRow_(0) {}
+	MatrixCSR(const int &row, const int &col);
+	MatrixCSR(const MatrixCSR &e);
+	~MatrixCSR() { if (col_ != nullptr) delete[]col_; if (value_ != nullptr) delete[]value_; if (rowIndex_ != nullptr) delete[]rowIndex_; };
+	void setValor(const int &row, const int &col, const float &value); // ha de redimensionar la matriu a row y col conservant els valors
+	bool getValor(const int &row, const int &col, float &value) const;
+	void init(const int &row, const int &col);
+	void resize(const int &row, const int &col);
+	friend std::ostream operator<<(std::ostream &a, const MatrixCSR &e);
+	MatrixCSR operator+(const MatrixCSR &e);
+	MatrixCSR operator-(const MatrixCSR &e);
+	MatrixCSR operator*(const MatrixCSR &e);
+	MatrixCSR operator*(const float &e);
+	MatrixCSR operator/(const float &e);
+	MatrixCSR operator=(const MatrixCSR &e);
 
+	inline int getNFiles()const { return nRow_; }
+	inline int getNColumnes()const { return nCol_; }
+	inline void setRow(const int &e) { nRow_ = e; }
+	inline void setCol(const int &e) { nCol_ = e; }
 private:
-	int *matrix_;
-	int row_, col_;
+	float *col_, *value_, *rowIndex_;// L'ultim valor de "rowIndex_" es el nombre total de valors diferents a zero
+	int sizeIndexRow_;
+	int nRow_, nCol_;
 };
+
+
 
