978b932d04cf502ad0a3c9dddd09ce756c0d27b5
p014 <pc165@pm.me>
2019-10-07 00:51:34 +0200

    matrix*vector

--- Matrix.cpp
+++ Matrix.cpp
@@ -5,16 +5,16 @@
 
 
 MatrixCSR::MatrixCSR() {
-	this->init(0, 0);
+	 this->init(0, 0);
 }
 
 
 MatrixCSR::MatrixCSR(const int &row, const int &col) {
-	this->init(row, col);
+	 this->init(row, col);
 }
 
 MatrixCSR::MatrixCSR(const MatrixCSR &e) {
-	this->copy(e);
+	 this->copy(e);
 }
 
 MatrixCSR::~MatrixCSR() {
@@ -22,171 +22,189 @@ MatrixCSR::~MatrixCSR() {
 }
 
 void MatrixCSR::copy(const MatrixCSR &e) {
-	nRow_ = e.nRow_;
-	nCol_ = e.nCol_;
-	rowIndex_ = e.rowIndex_;
-	columnValors_ = e.columnValors_;
+	 nRow_ = e.nRow_;
+	 nCol_ = e.nCol_;
+	 rowIndex_ = e.rowIndex_;
+	 columnValors_ = e.columnValors_;
 }
 
 
 void MatrixCSR::init(const int &row, const int &col) {
-	if (row < 0 || col < 0) throw "Error: Les columnes i files han de ser positius\n";
+	 if (row < 0 || col < 0) throw "Error: Les columnes i files han de ser positius\n";
 
-	nRow_ = row;
-	nCol_ = col;
-	rowIndex_.resize(nRow_ + 1);
-	//for (int i = 0; i < nRow_ + 1; i++)
-	//	rowIndex_[i] = 0;
+	 nRow_ = row;
+	 nCol_ = col;
+	 rowIndex_.resize(nRow_ + 1);
+	 //for (int i = 0; i < nRow_ + 1; i++)
+	 //	rowIndex_[i] = 0;
 }
 
 MatrixCSR MatrixCSR::operator+(const MatrixCSR &e) {
-	return MatrixCSR();
+	 return MatrixCSR();
 }
 
 MatrixCSR MatrixCSR::operator-(const MatrixCSR &e) {
-	return MatrixCSR();
+	 return MatrixCSR();
 }
 
 MatrixCSR MatrixCSR::operator*(const MatrixCSR &e) {
-	if (this->nCol_ != e.nRow_) throw "Producte invalid, el numero de files no es igual al de columnes";
-	MatrixCSR res(this->nRow_, e.nCol_);
-	float sum = 0;
+	 if (this->nCol_ != e.nRow_) throw "Producte invalid, el numero de files no es igual al de columnes";
+	 MatrixCSR res(this->nRow_, e.nCol_);
+	 float sum = 0;
 
-	for (int z = 0; z < nRow_; z++) {
-		for (int k = 0; k < nCol_; k++) {
-			for (int i = 0; i < nRow_; i++) {
-				for (int j = rowIndex_[i]; j < rowIndex_[i + 1]; j++) {
-					sum += columnValors_[j].data * e.columnValors_[columnValors_[j].col].data;
-				}
-			}
-			res.setValor(z, k, sum);
-			sum = 0;
-		}
-	}
+	 //for (int z = 0; z < nRow_; z++) {
+	 //	for (int k = 0; k < nCol_; k++) {
+	 //		for (int i = 0; i < nRow_; i++) {
+	 //			for (int j = rowIndex_[i]; j < rowIndex_[i + 1]; j++) {
+	 //				sum += columnValors_[j].data * e.columnValors_[columnValors_[j].col].data;
+	 //			}
+	 //		}
+	 //		res.setValor(z, k, sum);
+	 //		sum = 0;
+	 //	}
+	 //}
 
-	return res;
+	 return res;
 }
 
 std::vector<float> MatrixCSR::operator*(const std::vector<float> &e) {
-	if (this->nCol_ != e.size()) throw "Producte invalid, el numero de files no es igual al de columnes";
+	 if (this->nCol_ != e.size()) throw "Producte invalid, el numero de files no es igual al de columnes";
 
-	std::vector<float> result(this->nCol_, 0);
+	 std::vector<float> result(this->nRow_, 0);
 
-	for (int i = 0; i < nRow_; i++) {
-		for (int k = rowIndex_[i]; k < rowIndex_[i + 1]; k++) {
-			result[i] += columnValors_[k].data * e[columnValors_[k].col];
-		}
-	}
-	return result;
+	 for (int i = 0; i < nRow_; i++) {
+		  for (int k = rowIndex_[i]; k < rowIndex_[i + 1]; k++) {
+				std::cout << columnValors_[k].data << " * " << e[columnValors_[k].col] << "\n";
+				result[i] += columnValors_[k].data * e[columnValors_[k].col];
+		  }
+		  std::cout << "\n";
+	 }
+	 return result;
 }
 
 MatrixCSR MatrixCSR::operator/(const float &e) {
-	if (fabs(e) < FLT_EPSILON) throw "No es pot dividir per zero";
+	 if (fabs(e) < FLT_EPSILON) throw "No es pot dividir per zero";
 
-	MatrixCSR temp(*this);
-	for (int i = 0; i < this->rowIndex_[this->nRow_]; i++)
-		this->columnValors_[i].data /= e;
+	 MatrixCSR temp(*this);
+	 for (int i = 0; i < this->rowIndex_[this->nRow_]; i++)
+		  this->columnValors_[i].data /= e;
 
 
-	return temp;
+	 return temp;
 }
 
 
 void MatrixCSR::setValor(const int &row, const int &col, const float &value) {
-	if (row < 0 || col < 0) throw "Error: Els indexs son negatius";
-	if (fabs(value - 0.0f) < FLT_EPSILON) return;
+	 if (row < 0 || col < 0) throw "Error: Els indexs son negatius";
+	 if (fabs(value - 0.0f) < FLT_EPSILON) return;
 
 
-	if (!(row < nRow_ && col < nCol_)) {
-		int oldRow = nRow_;
-		nCol_ = col + 1;
-		if (!(row < nRow_)) {
-			nRow_ = row + 1;
-			rowIndex_.resize(row + 2);
-		}
+	 if (!(row < nRow_ && col < nCol_)) {
+		  int oldRow = nRow_;
+		  nCol_ = col + 1;
+		  if (!(row < nRow_)) {
+				nRow_ = row + 1;
+				rowIndex_.resize(row + 2);
+		  }
 
-		if (row == rowIndex_.size() - 2) { // si es l'ultima fila
-			for (int i = oldRow + 1; i < row + 2; i++)
-				rowIndex_[i] = rowIndex_[i - 1];
-			rowIndex_[row + 1]++;
-		} else {
-			for (int i = row + 1; i < rowIndex_.size(); i++)
-				rowIndex_[i]++;
-		}
+		  if (row == rowIndex_.size() - 2) { // si es l'ultima fila
+				for (int i = oldRow + 1; i < row + 2; i++)
+					 rowIndex_[i] = rowIndex_[i - 1];
+				rowIndex_[row + 1]++;
+		  } else {
+				for (int i = row + 1; i < rowIndex_.size(); i++)
+					 rowIndex_[i]++;
+		  }
 
-		columnValors_.insert(columnValors_.begin() + rowIndex_[row], { col,value });
-		std::sort(columnValors_.begin() + rowIndex_[row], columnValors_.begin() + rowIndex_[row + 1],
-				  [](const colVal &a, const colVal &b) {return a.col < b.col; });
+		  columnValors_.insert(columnValors_.begin() + rowIndex_[row], { col,value });
+		  std::sort(columnValors_.begin() + rowIndex_[row], columnValors_.begin() + rowIndex_[row + 1]);
 
-	} else {
-		bool found = false;
-		int i = rowIndex_[row];
-		while (i < rowIndex_[row + 1] && !found) {
-			if (columnValors_[i].col == col) {
-				columnValors_[i].data = value;
-				found = true;
-			}
-			i++;
-		}
-		if (!found) {
-			columnValors_.insert(columnValors_.begin() + rowIndex_[row], { col,value });
-			std::sort(columnValors_.begin() + rowIndex_[row], columnValors_.begin() + rowIndex_[row + 1],
-					  [](const colVal &a, const colVal &b) {return a.col < b.col; });
-			for (int i = row + 1; i < nRow_ + 1; i++)
-				rowIndex_[i]++;
+	 } else {
+		  bool found = false;
+		  int i = rowIndex_[row];
+		  while (i < rowIndex_[row + 1] && !found) {
+				if (columnValors_[i].col == col) {
+					 columnValors_[i].data = value;
+					 found = true;
+				}
+				i++;
+		  }
+		  if (!found) {
+				columnValors_.insert(columnValors_.begin() + rowIndex_[row], { col,value });
+				std::sort(columnValors_.begin() + rowIndex_[row], columnValors_.begin() + rowIndex_[row + 1]);
+				for (int i = row + 1; i < nRow_ + 1; i++)
+					 rowIndex_[i]++;
 
-		}
-	}
+		  }
+	 }
 }
 
 
 std::ostream &operator<<(std::ostream &a, const MatrixCSR &e) {
-	float colVal = 0;
-	for (int i = 0; i < e.nRow_; i++) {
-		for (int j = 0; j < e.nCol_; j++) {
-			e.getValor(i, j, colVal);
-			a << colVal << " ";
-		}
-		a << "\n";
-	}
-	a << "Matrix " << e.getNFiles() << "x" << e.getNColumnes() << "\n";
-	return a;
+	 float colVal = 0;
+	 for (int i = 0; i < e.nRow_; i++) {
+		  for (int j = 0; j < e.nCol_; j++) {
+				e.getValor(i, j, colVal);
+				if (colVal != 0)
+					 a << colVal;
+				else
+					 a << " ";
+				a << " ";
+		  }
+		  a << "\n";
+	 }
+
+	 a << "Matrix " << e.getNFiles() << "x" << e.getNColumnes() << "\n";
+	 for (auto &i : e.columnValors_) {
+		  a << "[" << i.col << "," << i.data << "] ";
+	 }
+	 a << "\n";
+
+	 for (auto &i : e.rowIndex_) {
+		  a << i << " ";
+	 }
+	 a << "\n";
+
+	 return a;
 }
 
 
 bool MatrixCSR::getValor(const int &row, const int &col, float &value) const {
-	if (row < 0 || col < 0) throw "Error: Els indexs son negatius";
+	 if (row < 0 || col < 0) throw "Error: Els indexs son negatius";
 
-	value = 0.0f;
-	if (row < nRow_ && col < nCol_)
-		for (int i = rowIndex_[row]; i < rowIndex_[row + 1]; i++)
-			if (columnValors_[i].col == col) {
-				value = columnValors_[i].data;
-				break;
-			}
+	 value = 0.0f;
+	 if (row < nRow_ && col < nCol_)
+		  for (int i = rowIndex_[row]; i < rowIndex_[row + 1]; i++)
+				if (columnValors_[i].col == col) {
+					 value = columnValors_[i].data;
+					 break;
+				}
 
-	return  (row < nRow_ && col < nCol_);
+	 return  (row < nRow_ && col < nCol_);
 }
 
 
 MatrixCSR &MatrixCSR::operator=(const MatrixCSR &e) {
-	this->copy(e);
-	return *this;
+	 this->copy(e);
+	 return *this;
 }
 
 
 void MatrixCSR::setRow(const int &e) {
-	if (e > nRow_ || e < 0) throw "Accio no permesa, utilitzi init per agrandar la matriu";
-	nRow_ = e;
+	 if (e > nRow_ || e < 0) throw "Accio no permesa, utilitzi init per agrandar la matriu";
+	 nRow_ = e;
 }
 
 void MatrixCSR::setCol(const int &e) {
-	if (e > nCol_ || e < 0) throw "Accio no permesa, utilitzi init per agrandar la matriu";
-	nCol_ = e;
+	 if (e > nCol_ || e < 0) throw "Accio no permesa, utilitzi init per agrandar la matriu";
+	 nCol_ = e;
 }
 
 void MatrixCSR::setRowCol(const int &a, const int &e) {
-	if (e > nCol_ || e < 0 || a>nRow_ || a < 0) throw "Accio no permesa, utilitzi init per agrandar la matriu";
-	nRow_ = a;
-	nCol_ = e;
+	 if (e > nCol_ || e < 0 || a>nRow_ || a < 0) throw "Accio no permesa, utilitzi init per agrandar la matriu";
+	 nRow_ = a;
+	 nCol_ = e;
+}
+bool operator<(const colVal &a, const colVal &b) {
+	 return a.col < b.col;
 }
--- main.cpp
+++ main.cpp
@@ -1,95 +1,31 @@
 #include <iostream>
 #include "Matrix.h"
 using std::cout;
+void multiplyMatrixVector() {
+	 MatrixCSR a;
+	 std::vector<std::vector<float>> matrix = {
+	 {0,2,3},
+	 {0,5,6},
+	 {0,8,0},
+	 {0,0,12},
+	 };
+	 std::vector<float> vector = { 1,0,1 };
+	 for (int i = 0; i < matrix.size(); i++)
+		  for (int j = 0; j < matrix[0].size(); j++)
+				a.setValor(i, j, matrix[i][j]);
+	 cout << a;
+	 std::vector<float> res = a * vector;
+
+	 for (auto &i : res)
+		  cout << i << " ";
+	 cout << "\n";
+}
+
 int main() {
-
-	//test
-	MatrixCSR a, b, c;
-	//a.init(10, 2);
-	a.setValor(9,0,1);
-	cout << a << "\n\n";
-	a.setValor(0,2,2);
-	cout << a << "\n\n";
-	a.setValor(0,8,8);
-	cout << a << "\n\n";
-	a.setValor(3,5,3);
-	cout << a << "\n\n";
-	a.setValor(3,0,1);
-	cout << a << "\n\n";
-	a.setValor(4,3,3);
-	cout << a << "\n\n";
-	a.setValor(8,5,5);
-	cout << a << "\n\n";
-	a.setValor(0,4,4);
-	cout << a << "\n\n";
-
-	/*
-	b = a;
-	b.setCol(1);
-	cout << b << "\n\n";
-
-	std::vector<std::vector<float>> matrix = {
-		{1,2,3,4,5,6,7,8},
-		{1,2,3,4,5,6,7,8},
-		{1,2,3,4,5,6,7,8},
-		{1,2,3,4,5,6,7,8},
-		{1,2,3,4,5,6,7,8},
-		{1,2,3,4,5,6,7,8},
-		{1,2,3,4,5,6,7,8},
-	};
-
-	for (int i = 0; i < matrix.size(); i++) {
-		for (int j = 0; j < matrix[0].size(); j++) {
-			c.setValor(i, j, matrix[i][j]);
-			//matrix[i][j] /= 3.0f;
-		}
-	}
-	cout << c << "\n\n";
-	float valor = 0.0f;
-	for (int i = 0; i < matrix.size(); i++) {
-		for (int j = 0; j < matrix[0].size(); j++) {
-			c.getValor(i, j, valor);
-			cout << std::boolalpha << (fabs(valor - matrix[i][j]) < FLT_EPSILON) << " ";
-		}
-		cout << "\n";
-	}
-	cout << "\n";
-
-	a = b = c;
-	b.setRowCol(2, 2);
-	c.setRowCol(7, 7);
-	cout << a << "\n\n " << b << "\n\n" << c << "\n\n";
-
-	c.init(4, 4);
-
-	cout << c;
-
-	std::vector<std::vector<float>> matrix2 = {
-		{1,2,3},
-		{1,2,3},
-		{1,2,3}
-	};
-
-	MatrixCSR v(matrix2.size(), matrix2[0].size());
-
-	for (int i = 0; i < matrix2.size(); i++) {
-		for (int j = 0; j < matrix2[0].size(); j++) {
-			v.setValor(i, j, matrix2[i][j]);
-		}
-	}
-	//v = v * v;
-	cout << v;
+	 multiplyMatrixVector();
 
 
-	std::vector<float> vector = { 1,2,3 };
 
-	//vector = v * vector;
 
-	//for (int i = 0; i < vector.size(); i++) {
-	//	cout << vector[i];
-	//}
-
-	*/
-
-	return 0;
+	 return 0;
 }
