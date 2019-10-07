6f4df09ebcd4a57759a0e545f2c2f4346f12068e
p014 <pc165@pm.me>
2019-10-07 13:07:36 +0200

    try/catch multiplyTest

--- main.cpp
+++ main.cpp
@@ -2,23 +2,27 @@
 #include "Matrix.h"
 using std::cout;
 void multiplyMatrixVector() {
-	 MatrixCSR a;
-	 std::vector<std::vector<float>> matrix = {
-	 {0,2,3},
-	 {0,5,6},
-	 {0,8,0},
-	 {0,0,12},
-	 };
-	 std::vector<float> vector = { 1,0,1 };
-	 for (int i = 0; i < matrix.size(); i++)
-		  for (int j = 0; j < matrix[0].size(); j++)
-				a.setValor(i, j, matrix[i][j]);
-	 cout << a;
-	 std::vector<float> res = a * vector;
+	 try {
+		  MatrixCSR a;
+		  std::vector<std::vector<float>> matrix = {
+		  {0,2,3},
+		  {0,5,6},
+		  {0,8,0},
+		  {0,0,12},
+		  };
+		  std::vector<float> vector = { 1,0,1 };
+		  for (int i = 0; i < matrix.size(); i++)
+				for (int j = 0; j < matrix[0].size(); j++)
+					 a.setValor(i, j, matrix[i][j]);
+		  cout << a;
+		  std::vector<float> res = a * vector;
 
-	 for (auto &i : res)
-		  cout << i << " ";
-	 cout << "\n";
+		  for (auto &i : res)
+				cout << i << " ";
+		  cout << "\n";
+	 } catch (const char *a) {
+		  cout << "Exception: " << a << "\n";
+	 }
 }
 
 int main() {
